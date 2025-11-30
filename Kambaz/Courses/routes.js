3c165708eed4d5b0e254e24fa990c834cde6102f
KD7789 <kpdevani2003@gmail.com>
2025-11-30 15:03:06 -0500

    6.4.3 Completed

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -74,6 +74,14 @@ export default function CourseRoutes(app, db) {
     const status = await enrollmentsDao.unenrollUserFromCourse(uid, cid);
     res.send(status);
   };
+
+  const findUsersForCourse = async (req, res) => {
+    const { cid } = req.params;
+    const users = await enrollmentsDao.findUsersForCourse(cid);
+    res.json(users);
+  }
+  app.get("/api/courses/:cid/users", findUsersForCourse);
+
   app.post("/api/users/:uid/courses/:cid", enrollUserInCourse);
   app.delete("/api/users/:uid/courses/:cid", unenrollUserFromCourse);
 
