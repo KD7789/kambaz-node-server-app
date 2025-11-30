2ad77d8ee107f44bb4a6951adea41e7261fa32f2
KD7789 <kpdevani2003@gmail.com>
2025-11-30 12:14:28 -0500

    6.4.2 Completed

--- Kambaz/Modules/dao.js
+++ Kambaz/Modules/dao.js
@@ -1,32 +1,47 @@
 import { v4 as uuidv4 } from "uuid";
+import model from "../Courses/model.js";
 
 export default function ModulesDao(db) {
-  function createModule(module) {
+  async function findModulesForCourse(courseId) {
+    const course = await model.findById(courseId);
+    return course.modules || [];
+  }
+
+  async function createModule(courseId, module) {
     const newModule = { ...module, _id: uuidv4() };
-    db.modules = [...db.modules, newModule];
+
+    await model.updateOne(
+      { _id: courseId },
+      { $push: { modules: newModule } }
+    );
+
     return newModule;
   }
 
-  function findModulesForCourse(courseId) {
-    return db.modules.filter((m) => m.course === courseId);
-  }
-
-  function deleteModule(moduleId) {
-    const { modules } = db;
-    db.modules = modules.filter((m) => m._id !== moduleId);
+  async function deleteModule(courseId, moduleId) {
+    await model.updateOne(
+      { _id: courseId },
+      { $pull: { modules: { _id: moduleId } } }
+    );
     return { status: "ok" };
   }
 
-  function updateModule(moduleId, moduleUpdates) {
-    const module = db.modules.find((m) => m._id === moduleId);
+  async function updateModule(moduleId, courseId, moduleUpdates) {
+    const course = await model.findById(courseId);
+    if (!course) return null;
+  
+    const module = course.modules.id(moduleId);
     if (!module) return null;
+  
     Object.assign(module, moduleUpdates);
+  
+    await course.save();
     return module;
-  }
+  }  
 
   return {
-    createModule,
     findModulesForCourse,
+    createModule,
     deleteModule,
     updateModule,
   };
